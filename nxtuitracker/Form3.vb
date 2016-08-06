b50494b1343bd97b9c91ad06e092ce07b3071daf
Doomtrain14 <yet.ebreo@gmail.com>
2016-08-07 02:02:25 +0800

    Added new features
    wafer definition
    multiple groupings
    show grid lines etc

--- nxtuitracker/Form3.vb
+++ nxtuitracker/Form3.vb
@@ -2,8 +2,8 @@
 
     Private Sub frm_mini_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Me.Opacity = 0.6
-        Me.Left = Screen.PrimaryScreen.WorkingArea.Width - Me.Width
-        Me.Top = Screen.PrimaryScreen.WorkingArea.Height - Me.Height - 50
+        Me.Left = Screen.PrimaryScreen.WorkingArea.Width - Me.Width - 30
+        Me.Top = Screen.PrimaryScreen.WorkingArea.Height - Me.Height - 75
 
         Me.ShowInTaskbar = False
 
